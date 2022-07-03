3ab4403f2be555a0c114fee9843a1320f009eceb
Noah Goss <95068655+RainbowCitrine@users.noreply.github.com>
2022-07-03 15:16:40 -0700

    Create TimeAndSpace.cpp

---
+++ Mid Term Practice/TimeAndSpace.cpp
@@ -0,0 +1,48 @@
+Int sum(int A[], int n)
+{
+
+	int s, I;
+	s = 0; — 1 time
+		
+		1.    N + 1  n
+	for(I = 0; I <n; ++I) — 2(n + 1)
+	{
+	
+		s = s + A[I]; — N (How many times loop will run)
+
+	}
+
+
+Return s;  — 1 time 
+
+	f(n) = 2n ^1+ 3
+	O(n)
+}
+
+
+Void add(int n) 
+{
+
+	int I, j; 
+	for(I =0; I < n; ++I) — n + 1
+	{
+	for(j = 0; j < n; ++j) —- n *(n+1)
+	{ 		c[I][j] = A[I][j] + B[I][j]; n x n
+	}
+
+Two for loops will make this algorithm O(n^2)
+
+
+}
+
+
+
+func()
+
+{ 
+	func() — n 
+
+
+}
+
+O(n) since there is one loop 
