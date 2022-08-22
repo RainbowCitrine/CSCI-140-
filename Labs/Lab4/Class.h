e245794b9e7c33435fd475c4648f7d2a560df126
Noah Goss <95068655+RainbowCitrine@users.noreply.github.com>
2022-08-21 18:04:31 -0700

    Create Class.h

---
+++ Labs/Lab4/Class.h
@@ -0,0 +1,33 @@
+class Calculator
+
+{
+
+private:
+
+int input1;
+
+int input2;
+
+public:
+
+Calculator();
+
+Calculator(int,int);
+
+int addition();
+
+int subtraction();
+
+int multiplication();
+
+float division();
+
+int getinput1();
+
+int getinput2();
+
+void setinput1(int);
+
+void setinput2(int);
+
+};
